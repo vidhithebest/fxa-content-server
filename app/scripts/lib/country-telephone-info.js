3fe2c56a8d497f1728c790fb1d77e3fded295bed
Shane Tomlinson <stomlinson@mozilla.com>
2017-10-18 12:59:29 +0100

    chore(sms): Re-format country-telephone-info.js
    Everything was 1 space off where it should be.
    
    Not attached to an issue.

--- app/scripts/lib/country-telephone-info.js
+++ app/scripts/lib/country-telephone-info.js
@@ -5,8 +5,8 @@
  /**
   * Country->phone number info.
   */
- define((require, exports, module) => {
-   'use strict';
+define((require, exports, module) => {
+  'use strict';
 
    /**
     * Each country entry should have the fields listed below.
@@ -41,60 +41,59 @@
     * @type {String}
     */
 
-   module.exports = {
-     GB: {
-       format (num) {
-         // +44 1234 567890
-         return num.slice(0, 3) + ' ' + num.slice(3, 7) + ' ' + num.slice(7, 14);
-       },
-       normalize (num) {
-         if (/^\+44/.test(num)) {
-           return num;
-         }
-         return `+44${num}`;
-       },
-       pattern: /^(?:\+44)?\d{10,10}$/,
-       prefix: '+44'
-     },
-     RO: {
-       format(num) {
-         // +40 7xx xxxxxx
-         return num.slice(0, 3) + ' ' + num.slice(3, 6) + ' ' + num.slice(6, 12);
-       },
-       normalize(num) {
-         // allow +40 country code prefix
-         // as well as an extra 0 before the 7 prefix.
-         const prefix = /^(\+40)?0?/;
-         if (prefix.test(num)) {
-           num = num.replace(prefix, '');
-         }
-         return `+40${num}`;
-       },
-       // +407xxxxxxxx, allow leading 0 for sloppiness.
-       pattern: /^(?:\+40)?0?7\d{8,8}$/,
-       // The country code is +40, all mobile phones have a 7 prefix.
-       prefix: '+407'
+  module.exports = {
+    GB: {
+      format (num) {
+        // +44 1234 567890
+        return num.slice(0, 3) + ' ' + num.slice(3, 7) + ' ' + num.slice(7, 14);
+      },
+      normalize (num) {
+        if (/^\+44/.test(num)) {
+          return num;
+        }
+        return `+44${num}`;
+      },
+      pattern: /^(?:\+44)?\d{10,10}$/,
+      prefix: '+44'
+    },
+    RO: {
+      format(num) {
+        // +40 7xx xxxxxx
+        return num.slice(0, 3) + ' ' + num.slice(3, 6) + ' ' + num.slice(6, 12);
+      },
+      normalize(num) {
+        // allow +40 country code prefix
+        // as well as an extra 0 before the 7 prefix.
+        const prefix = /^(\+40)?0?/;
+        if (prefix.test(num)) {
+          num = num.replace(prefix, '');
+        }
+        return `+40${num}`;
+      },
+      // +407xxxxxxxx, allow leading 0 for sloppiness.
+      pattern: /^(?:\+40)?0?7\d{8,8}$/,
+      // The country code is +40, all mobile phones have a 7 prefix.
+      prefix: '+407'
+    },
+    US: {
+      format (num) {
+        // Americans don't use country codes, drop the country code prefix
+        // 123-456-7890
+        return num.slice(2, 5) + '-' + num.slice(5, 8) + '-' + num.slice(8);
+      },
+      normalize (num) {
+        if (/^\+1/.test(num)) {
+          return num;
+        } else if (/^1/.test(num) && num.length === 11) {
+          return `+${num}`;
+        }
+        return `+1${num}`;
+      },
+      pattern: /^(\+?1)?[2-9]\d{9,9}$/, // allow for a +1 or 1 prefix before the area code, area codes are all 2-9
+      prefix: '+1'
+    }
+  };
 
-     },
-     US: {
-       format (num) {
-         // Americans don't use country codes, drop the country code prefix
-         // 123-456-7890
-         return num.slice(2, 5) + '-' + num.slice(5, 8) + '-' + num.slice(8);
-       },
-       normalize (num) {
-         if (/^\+1/.test(num)) {
-           return num;
-         } else if (/^1/.test(num) && num.length === 11) {
-           return `+${num}`;
-         }
-         return `+1${num}`;
-       },
-       pattern: /^(\+?1)?[2-9]\d{9,9}$/, // allow for a +1 or 1 prefix before the area code, area codes are all 2-9
-       prefix: '+1'
-     }
-   };
-
-   // alias CA (Canada) to use the same info as the US.
-   module.exports.CA = module.exports.US;
- });
+  // alias CA (Canada) to use the same info as the US.
+  module.exports.CA = module.exports.US;
+});
